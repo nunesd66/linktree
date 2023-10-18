f5af1323b64eaaa8d0cc83d2131b86c8877c7483
Daniel Nunes <nunes@CRP-DESEN-MG03.crp.com>
2023-10-18 08:19:59 -0300

    project linktree

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const light = 'light';
+  const html = document.documentElement;
+  html.classList.toggle(light);
+  
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains(light)) {
+    img.setAttribute('src', './assets/avatar-light.png');
+    img.setAttribute('alt', 'descrição com tema em light mode');
+  } else {
+    img.setAttribute('src', './assets/avatar.png');
+    img.setAttribute('alt', 'descrição com tema em dark mode');
+  }
+}
